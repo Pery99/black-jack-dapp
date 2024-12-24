6fbc7b84d8a1b100a65d697c616cc5976679bfe8
pery <oluwapery99@gmail.com>
2024-12-24 06:29:59 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      include: ['buffer', 'crypto', 'stream', 'util']
-    })
+      include: ["buffer", "crypto", "stream", "util"],
+    }),
   ],
   define: {
-    'process.env': {},
-    global: {}
+    "process.env": {},
+    global: {},
   },
   resolve: {
     alias: {
-      stream: 'stream-browserify',
-      buffer: 'buffer'
-    }
-  }
+      stream: "stream-browserify",
+      buffer: "buffer",
+    },
+  },
 });
